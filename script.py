c8c28aabe126e28c68b4ee5d0f5f70740b22500b
Daniel A <dreth@pop-os.localdomain>
2020-08-14 22:09:39 -0400

    fixing tree and gitignore

--- script.py
+++ script.py
@@ -19,23 +19,21 @@ client = translate.TranslationServiceClient()
 def translate_txt(text, project_id):
     """Translating Text using google API"""
 
-    # Detail on supported types can be found here:
     response = client.translate_text(
         parent=f'projects/{project_id}',
         contents=[text],
-        mime_type="text/plain",  # mime types: text/plain, text/html
+        mime_type="text/plain",
         source_language_code="nl",
         target_language_code="en-US",
     )
-
-    # Display the translation for each input text provided
+    
     return response
 
 
 # importing data
-subtlexnl = pd.read_csv('subtlex-nl-v1.3/SUBTLEX-NL.cd-above2.txt', sep='\t')
-wordlexnl1 = pd.read_csv('wordlex-nl/Nl.Freq.2.txt', sep='\t')
-wordlexnl2 = pd.read_csv('wordlex-nl/Nl.Freq.3.Hun.txt', sep='\t')
+subtlexnl = pd.read_csv('data/subtlex-nl-v1.3/SUBTLEX-NL.cd-above2.txt', sep='\t')
+wordlexnl1 = pd.read_csv('data/wordlex-nl/Nl.Freq.2.txt', sep='\t')
+wordlexnl2 = pd.read_csv('data/wordlex-nl/Nl.Freq.3.Hun.txt', sep='\t')
 
 # joining wordlexnl1 and wordlexnl2
 wordlexnl = pd.merge(wordlexnl1, wordlexnl2, how='inner', on=['Word','BlogCDPc', 'TwitterCDPc', 'NewsCDPc'])
@@ -58,12 +56,21 @@ weights = {
     'TwitterCDPc':1,
     'NewsCDPc':0.6,
 }
-for col, mult in weights.items():
+for n, (col, mult) in enumerate(weights.items()):
     scaler = MinMaxScaler()
     wordlexnl[col] = scaler.fit_transform(wordlexnl[[col]])*mult
+    if n == len(weights.items()):
+        scaler = MinMaxScaler()
+        subtlexnl['rel_freq_sl'] = scaler.fit_transform(subtlexnl[['rel_freq_sl']])
+
+# dropping additional cols and creating the rel freq col
 wordlexnl['rel_freq_wl'] = wordlexnl[list(weights.keys())].sum(axis=1)
+wordlexnl = wordlexnl[['word', 'rel_freq_wl']]
 
 # merging only common word entries
-data = pd.merge(subtlexnl, wordlexnl, how='inner', on='word')[['word','rel_freq_sl','rel_freq_wl']]
+data = pd.merge(subtlexnl, wordlexnl, how='inner', on='word')
+data['rel_freq'] = data['rel_freq_sl'] + data['rel_freq_wl']
+data = data[['word','rel_freq']].sort_values('rel_freq', ascending=False)
+data = data.reset_index()[['word']]
 
-# weights
+# 
