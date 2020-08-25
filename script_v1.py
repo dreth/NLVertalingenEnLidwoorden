e3a04570e97b5ad674428a2544da41dd07d6f0f4
Daniel A <dreth@pop-os.localdomain>
2020-08-25 16:35:07 +0200

    fixing accents data

--- script_v1.py
+++ script_v1.py
@@ -17,7 +17,7 @@ environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'api-key.json'
 # project number (google cloud project)
 # I suggest you add this as an entry to your api key json
 with open('api-key.json') as apikey:
-    project_number = load(apikey)
+    project_number = load(apikey)['project_number']
 
 # using google api
 def translate_txt(text, project_id):
@@ -34,6 +34,19 @@ def translate_txt(text, project_id):
     )
     
     return response.translations[0].translated_text
+
+def detect_lang(text, project_id):
+    """Translating Text using google API"""
+
+    client = translate.TranslationServiceClient()
+
+    response = client.detect_language(
+        parent=f'projects/{project_id}',
+        content=text,
+        mime_type="text/plain"
+    )
+    
+    return response.languages[0].language_code
 
 
 # importing data
@@ -89,19 +102,6 @@ classif = classif[(classif['Lemma'] != '@') & (classif['Lemma'].isin(data['word'
 # filtering 1 letter words
 classif = classif[classif['Lemma'].isin([x for x in classif['Lemma'] if len(x) > 1])]
 
-# checking if words include accents, if so only keep those accented words
-# that are french loanwords or include the words 'privé', 'één', 'vóór'
-accented_words = classif[0:0]
-accents = ['á', 'é', 'í', 'ó', 'ú']
-no_accents = ['a', 'e', 'i', 'o', 'u']
-for letter in accents:
-    accented_words = pd.concat([accented_words, classif[classif['Lemma'].str.contains(letter)]])
-for word in accented_words['Lemma']:
-    new_word = word
-    for x,y in zip(accents, no_accents):
-        new_word = new_word.replace(x, y)
-    accented_words.loc[accented_words['Lemma'] == word, 'unac_eq'] = new_word
-
 # filtering 2 letter words and keeping them if they have a meaning in english or a translation
 # such translation must also not be identical, otherwise the word will be removed from the main list
 two_letter_words = set([word for word in classif['Lemma'] if len(word) < 4])
@@ -115,19 +115,45 @@ low_length_eval = pd.DataFrame(low_length_eval)
 low_length_eval_remove = low_length_eval[(low_length_eval['meaning'].isna())
                                             & (low_length_eval['word'] == low_length_eval['translation'])]
 classif = classif[~classif['Lemma'].isin(low_length_eval_remove['word'])]
- 
+
+# checking if words include accents, if so only keep those accented words
+# that are french loanwords or include the words 'privé', 'één', 'vóór'
+accented_words = classif[0:0]
+accents = ['á', 'é', 'í', 'ó', 'ú']
+no_accents = ['a', 'e', 'i', 'o', 'u']
+for letter in accents:
+    accented_words = pd.concat([accented_words, classif[classif['Lemma'].str.contains(letter)]])
+accented_words = accented_words[~((accented_words['Lemma'].str.contains('privé'))
+                                    | (accented_words['Lemma'].str.contains('één'))
+                                    | (accented_words['Lemma'].str.contains('vóór')))]
+kept_accented_words = []
+dataset_without_accented_words = classif[~classif['Lemma'].isin(accented_words)]['Lemma']
+for word in accented_words['Lemma'].unique():
+    if detect_lang(word, project_number) == 'fr':
+        kept_accented_words.append(word)
+    else:
+        print('2nd')
+        for x,y in zip(accents, no_accents):
+            if x in word:
+                new_word = word.replace(x, y)
+                print('new: ', new_word)
+                print('orig: ', word)
+                if new_word not in dataset_without_accented_words:
+                    kept_accented_words.append(word)
+excluded_accented_words = accented_words[~accented_words['Lemma'].isin(kept_accented_words)]['Lemma'].unique()
+classif = classif[~classif['Lemma'].isin(excluded_accented_words)]
 
 # nouns and verbs filter
-nw_ww = classif[classif['POS'].isin(['N','WW'])]
-nw_ww_VC = nw_ww['Lemma'].value_counts()
+nw_ww_adj = classif[classif['POS'].isin(['N','WW'])]
+nw_ww_adj_VC = nw_ww_adj['Lemma'].value_counts()
 
 # importing  spacy's nl_core_news_lg model
 sp = spacy.load('nl_core_news_lg')
 
 # extracting verbs only from previous filter
 onr_verben = ['gaan', 'slaan', 'staan', 'zijn']
-verben = []
-for word in nw_ww_VC[nw_ww_VC > 1].index:
+verben, adjectieven = [], []
+for word in nw_ww_adj_VC[nw_ww_adj_VC > 1].index:
     # if the word includes an irregular verb from the irr verb list in their last 5 chars, add to verb list
     cond = tuple((stem in word[-5:]) for stem in onr_verben)
     if any(cond):
@@ -137,18 +163,30 @@ for word in nw_ww_VC[nw_ww_VC > 1].index:
     # then add to verb list
     elif word[-2:] == 'en':
         zin = sp(f'ik ga {word}')
+        zin2 = sp(f'mijn huis is {word}')
         if zin[2].pos_ == 'VERB':
             verben.append(word)
+        elif zin2[3].pos_ == 'ADJ':
+            adjectieven.append(word)
+    else:
+        zin2 = sp(f'mijn huis is {word}')
+        if zin2[3].pos_ == 'ADJ':
+            adjectieven.append(word)
 
 # Nouns
-naamwoorden = nw_ww[(~(nw_ww['Lemma'].isin(verben)) |
-                (nw_ww['Lemma'].isin(nw_ww_VC[nw_ww_VC == 1].index)))
-                & (nw_ww['POS'] == 'N')]
+naamwoorden = nw_ww_adj[(~(nw_ww_adj['Lemma'].isin(verben)) |
+                ((nw_ww_adj['Lemma'].isin(nw_ww_adj_VC[nw_ww_adj_VC == 1].index)))
+                & (nw_ww_adj['POS'] == 'N'))]
 
 # Verbs
-verben = nw_ww[((nw_ww['Lemma'].isin(verben)) | 
-                (nw_ww['Lemma'].isin(nw_ww_VC[nw_ww_VC == 1].index)))
-                & (nw_ww['POS'] == 'WW')]
+verben = nw_ww_adj[((nw_ww_adj['Lemma'].isin(verben)) | 
+                ((nw_ww_adj['Lemma'].isin(nw_ww_adj_VC[nw_ww_adj_VC == 1].index)))
+                & (nw_ww_adj['POS'] == 'WW'))]
+
+# Adjectives
+adjectieven = nw_ww_adj[((nw_ww_adj['Lemma'].isin(adjectieven)) | 
+                ((nw_ww_adj['Lemma'].isin(nw_ww_adj_VC[nw_ww_adj_VC == 1].index)))
+                & (nw_ww_adj['POS'] == 'ADJ'))]
 
 woorden = pd.concat([verben, naamwoorden])
 
