2673da534549a8a50eebaed44709b7cea2273286
Daniel A <dreth@pop-os.localdomain>
2020-08-28 00:59:37 +0200

    adding pos reference

--- script_v1.py
+++ script_v1.py
@@ -22,6 +22,8 @@ with open('api-key.json') as apikey:
 
 # %% Functions defined to translate and detect language
 # using google api
+
+
 def translate_txt(text, project_id, batch=False):
     """
     Translating Text using google API
@@ -31,10 +33,10 @@ def translate_txt(text, project_id, batch=False):
 
     client = translate.TranslationServiceClient()
     params = {
-        'parent':f'projects/{project_id}',
-        'mime_type':"text/plain",
-        'source_language_code':"nl",
-        'target_language_code':"en-US"
+        'parent': f'projects/{project_id}',
+        'mime_type': "text/plain",
+        'source_language_code': "nl",
+        'target_language_code': "en-US"
     }
 
     if batch == False:
@@ -46,7 +48,8 @@ def translate_txt(text, project_id, batch=False):
         params['contents'] = text
         response = client.translate_text(**params)
         return [response.translations[x].translated_text for x in range(len(response.translations))]
-    
+
+
 def detect_lang(text, project_id):
     """Translating Text using google API"""
 
@@ -57,44 +60,49 @@ def detect_lang(text, project_id):
         content=text,
         mime_type="text/plain"
     )
-    
+
     return response.languages[0].language_code
 
 
 # %% Importing data
-subtlexnl = pd.read_csv('data/subtlex-nl-v1.3/SUBTLEX-NL.cd-above2.txt', sep='\t')
+subtlexnl = pd.read_csv(
+    'data/subtlex-nl-v1.3/SUBTLEX-NL.cd-above2.txt', sep='\t')
 wordlexnl1 = pd.read_csv('data/wordlex-nl/Nl.Freq.2.txt', sep='\t')
 wordlexnl2 = pd.read_csv('data/wordlex-nl/Nl.Freq.3.Hun.txt', sep='\t')
 
 # %% Manipulating data
 # joining wordlexnl1 and wordlexnl2
-wordlexnl = pd.merge(wordlexnl1, wordlexnl2, how='inner', on=['Word','BlogCDPc', 'TwitterCDPc', 'NewsCDPc'])
+wordlexnl = pd.merge(wordlexnl1, wordlexnl2, how='inner', on=[
+                     'Word', 'BlogCDPc', 'TwitterCDPc', 'NewsCDPc'])
 
 # relative frequency of subtlexnl
 total_occ = subtlexnl['FREQcount'].sum()
-subtlexnl['rel_freq_sl'] = subtlexnl['FREQcount'].apply(lambda x: (x/total_occ)*100)
+subtlexnl['rel_freq_sl'] = subtlexnl['FREQcount'].apply(
+    lambda x: (x/total_occ)*100)
 
 # keeping only useful columns and renaming
 cols = {
-    'Word':'word',
+    'Word': 'word',
 }
-subtlexnl = subtlexnl[['Word','rel_freq_sl']].rename(columns=cols)
-wordlexnl = wordlexnl[['Word','BlogCDPc', 'TwitterCDPc', 'NewsCDPc']].rename(columns=cols)
+subtlexnl = subtlexnl[['Word', 'rel_freq_sl']].rename(columns=cols)
+wordlexnl = wordlexnl[['Word', 'BlogCDPc',
+                       'TwitterCDPc', 'NewsCDPc']].rename(columns=cols)
 
 # assigning weights to the types of frequencies
 weights = {
-    'BlogCDPc':0.8,
-    'TwitterCDPc':1,
-    'NewsCDPc':0.6,
+    'BlogCDPc': 0.8,
+    'TwitterCDPc': 1,
+    'NewsCDPc': 0.6,
 }
 
-# normalizing columns 
+# normalizing columns
 for n, (col, mult) in enumerate(weights.items()):
     scaler = MinMaxScaler()
     wordlexnl[col] = scaler.fit_transform(wordlexnl[[col]])*mult
     if n == len(weights.items()):
         scaler = MinMaxScaler()
-        subtlexnl['rel_freq_sl'] = scaler.fit_transform(subtlexnl[['rel_freq_sl']])
+        subtlexnl['rel_freq_sl'] = scaler.fit_transform(
+            subtlexnl[['rel_freq_sl']])
 
 # dropping additional cols and creating the rel freq col
 wordlexnl['rel_freq_wl'] = wordlexnl[list(weights.keys())].sum(axis=1)
@@ -103,56 +111,80 @@ wordlexnl = wordlexnl[['word', 'rel_freq_wl']]
 # merging only common word entries between subtlex and wordlex
 data = pd.merge(subtlexnl, wordlexnl, how='inner', on='word')
 data['rel_freq'] = data['rel_freq_sl'] + data['rel_freq_wl']
-data = data[['word','rel_freq']].sort_values('rel_freq', ascending=False)
+data = data[['word', 'rel_freq']].sort_values('rel_freq', ascending=False)
 data = data.reset_index()[['word']]
 
 # %% Tagging part of speech
 # adding tag to determine type of word in order to add articles to nouns
 classif1 = pd.read_csv('data/subtlex-nl-v1.3/SUBTLEX-NL.master.txt', sep='\t')
-classif2 = pd.read_csv('data/subtlex-nl-v1.3/SUBTLEX-NL.master.cd-above2.txt', sep='\t')
-classif = pd.concat([classif1,classif2])[['Lemma','POS']]
-classif = classif[(classif['Lemma'] != '@') & (classif['Lemma'].isin(data['word']))].drop_duplicates()
+classif2 = pd.read_csv(
+    'data/subtlex-nl-v1.3/SUBTLEX-NL.master.cd-above2.txt', sep='\t')
+classif = pd.concat([classif1, classif2])[['Lemma', 'POS']]
+classif = classif[(classif['Lemma'] != '@') & (
+    classif['Lemma'].isin(data['word']))].drop_duplicates()
 classif['meaning_en'] = np.nan
+
+# %% Part of speech classsification for reference purposes
+Part_of_speech = {'N': 'Noun',
+                  'SPEC': 'Special',
+                  'VG': 'Conjunction',
+                  'ADJ': 'Adjective',
+                  'WW': 'Verb',
+                  'VZ': 'Preposition',
+                  'BW': 'Adverb',
+                  'VNW': 'Pronoun',
+                  'TW': 'Numeral',
+                  'TSW': 'Interjection',
+                  'LID': 'Determiner'}
 
 # %% Filtering Articles (lidwoord)
 # listing all possible articles
 lidwoorden = ['de', 'het', 'een']
 lidwoorden_df = classif[classif['POS'] == 'LID']
-classif.loc[classif['POS'] == 'LID'] = classif[(classif['POS'] == 'LID') & (classif['Lemma'].isin(lidwoorden))]
+classif.loc[classif['POS'] == 'LID'] = classif[(
+    classif['POS'] == 'LID') & (classif['Lemma'].isin(lidwoorden))]
 
 # de
-classif.loc[(classif['Lemma'] == 'de') & (classif['POS'] == 'LID'), 'meaning_en'] = 'the (definite article, for de-woorden)'
+classif.loc[(classif['Lemma'] == 'de') & (classif['POS'] == 'LID'),
+            'meaning_en'] = 'the (definite article, for de-woorden)'
 
 # een
-classif.loc[(classif['Lemma'] == 'een') & (classif['POS'] == 'LID'), 'meaning_en'] = 'a/an (indefinite article)'
+classif.loc[(classif['Lemma'] == 'een') & (classif['POS'] == 'LID'),
+            'meaning_en'] = 'a/an (indefinite article)'
 
 # het (also as pronoun)
-classif.loc[(classif['Lemma'] == 'het') & (classif['POS'] == 'LID'), 'meaning_en'] = 'the (definite article, for het-woorden)'
-classif.loc[(classif['Lemma'] == 'het') & (classif['POS'] == 'VNW'), 'meaning_en'] = 'it'
+classif.loc[(classif['Lemma'] == 'het') & (classif['POS'] == 'LID'),
+            'meaning_en'] = 'the (definite article, for het-woorden)'
+classif.loc[(classif['Lemma'] == 'het') & (
+    classif['POS'] == 'VNW'), 'meaning_en'] = 'it'
 
-# finally, replacing entries labeled as lidwoord 
-classif.loc[classif['Lemma'].isin(lidwoorden)] = classif[~classif['meaning_en'].isna()]
+# finally, replacing entries labeled as lidwoord
+classif.loc[classif['Lemma'].isin(
+    lidwoorden)] = classif[~classif['meaning_en'].isna()]
 
 # cleaning up removed things
-classif = classif[~(classif['POS'].isna())] 
+classif = classif[~(classif['POS'].isna())]
 
 # %% Filtering 1 letter words
-classif = classif[classif['Lemma'].isin([x for x in classif['Lemma'] if len(x) > 1])]
+classif = classif[classif['Lemma'].isin(
+    [x for x in classif['Lemma'] if len(x) > 1])]
 
 # %% Filtering 2 and 3 letter words
 # and keeping them if they have a meaning in english or a translation
 # such translation must also not be identical, otherwise the word will be removed from the main list
-two_letter_words = list(set([word for word in classif['Lemma'] if len(word) < 4]))
+two_letter_words = list(
+    set([word for word in classif['Lemma'] if len(word) < 4]))
 # batch translating to find the meanings in the translation key of the dictionary
-low_length_eval = {'word':two_letter_words, 
-                    'translation':translate_txt(two_letter_words, project_number, batch=True)} 
+low_length_eval = {'word': two_letter_words,
+                   'translation': translate_txt(two_letter_words, project_number, batch=True)}
 # finding english meanings
-low_length_eval['meaning_en'] = [dictionary.meaning(word) for word in low_length_eval['translation']]
+low_length_eval['meaning_en'] = [dictionary.meaning(
+    word) for word in low_length_eval['translation']]
 # converting to df and filtering words that do NOT have a meaning from pydictionary and those that are
 # equal to the translation in order to make a list to remove those elements
 low_length_eval = pd.DataFrame(low_length_eval)
 low_length_eval_remove = low_length_eval[(low_length_eval['meaning_en'].isna())
-                                            & (low_length_eval['word'] == low_length_eval['translation'])]
+                                         & (low_length_eval['word'] == low_length_eval['translation'])]
 # removing elements by filtering these out
 classif = classif[~classif['Lemma'].isin(low_length_eval_remove['word'])]
 
@@ -163,21 +195,23 @@ accented_words = classif[0:0]
 accents = ['á', 'é', 'í', 'ó', 'ú']
 no_accents = ['a', 'e', 'i', 'o', 'u']
 for letter in accents:
-    accented_words = pd.concat([accented_words, classif[classif['Lemma'].str.contains(letter)]])
+    accented_words = pd.concat(
+        [accented_words, classif[classif['Lemma'].str.contains(letter)]])
 
 # filtering most common accented words
 accented_words = accented_words[~((accented_words['Lemma'].str.contains('privé'))
-                                    | (accented_words['Lemma'].str.contains('één'))
-                                    | (accented_words['Lemma'].str.contains('vóór')))]
+                                  | (accented_words['Lemma'].str.contains('één'))
+                                  | (accented_words['Lemma'].str.contains('vóór')))]
 kept_accented_words = []
-dataset_without_accented_words = classif[~classif['Lemma'].isin(accented_words)]['Lemma'].unique()
+dataset_without_accented_words = classif[~classif['Lemma'].isin(
+    accented_words)]['Lemma'].unique()
 
 for word in accented_words['Lemma'].unique():
     # detecting if the word is originally french and adding to list to keep
     if detect_lang(word, project_number) == 'fr':
         kept_accented_words.append(word)
     else:
-        for x,y in zip(accents, no_accents):
+        for x, y in zip(accents, no_accents):
             if x in word:
                 # replacing accent in the word with unaccented character
                 new_word = word.replace(x, y)
@@ -186,7 +220,8 @@ for word in accented_words['Lemma'].unique():
                 if new_word not in dataset_without_accented_words:
                     kept_accented_words.append(word)
 # listing all excluded words
-excluded_accented_words = accented_words[~accented_words['Lemma'].isin(kept_accented_words)]['Lemma'].unique()
+excluded_accented_words = accented_words[~accented_words['Lemma'].isin(
+    kept_accented_words)]['Lemma'].unique()
 # filtering dataset by excluded words
 classif = classif[~classif['Lemma'].isin(excluded_accented_words)]
 
@@ -195,7 +230,7 @@ sp = spacy.load('nl_core_news_lg')
 
 # %% Filtering verbs, nouns and adjectives
 # nouns, verbs and adjectives filter
-nw_ww_adj = classif[classif['POS'].isin(['N','WW','ADJ'])]
+nw_ww_adj = classif[classif['POS'].isin(['N', 'WW', 'ADJ'])]
 nw_ww_adj_VC = nw_ww_adj['Lemma'].value_counts()
 
 # extracting verbs only from previous filter
@@ -225,22 +260,21 @@ for word in nw_ww_adj_VC[nw_ww_adj_VC > 1].index:
 
 # Nouns
 naamwoorden = nw_ww_adj[(~(nw_ww_adj['Lemma'].isin(verben)) |
-                (nw_ww_adj['Lemma'].isin(nw_ww_adj_VC[nw_ww_adj_VC == 1].index)))
-                & (nw_ww_adj['POS'] == 'N')]
+                         (nw_ww_adj['Lemma'].isin(nw_ww_adj_VC[nw_ww_adj_VC == 1].index)))
+                        & (nw_ww_adj['POS'] == 'N')]
 
 # Verbs
-verben = nw_ww_adj[((nw_ww_adj['Lemma'].isin(verben)) | 
-                (nw_ww_adj['Lemma'].isin(nw_ww_adj_VC[nw_ww_adj_VC == 1].index)))
-                & (nw_ww_adj['POS'] == 'WW')]
+verben = nw_ww_adj[((nw_ww_adj['Lemma'].isin(verben)) |
+                    (nw_ww_adj['Lemma'].isin(nw_ww_adj_VC[nw_ww_adj_VC == 1].index)))
+                   & (nw_ww_adj['POS'] == 'WW')]
 
 # Adjectives
-adjectieven = nw_ww_adj[((nw_ww_adj['Lemma'].isin(adjectieven)) | 
-                (nw_ww_adj['Lemma'].isin(nw_ww_adj_VC[nw_ww_adj_VC == 1].index)))
-                & (nw_ww_adj['POS'] == 'ADJ')]
+adjectieven = nw_ww_adj[((nw_ww_adj['Lemma'].isin(adjectieven)) |
+                         (nw_ww_adj['Lemma'].isin(nw_ww_adj_VC[nw_ww_adj_VC == 1].index)))
+                        & (nw_ww_adj['POS'] == 'ADJ')]
 
 # joining all word types
 woorden = pd.concat([verben, naamwoorden, adjectieven])
-
 
 
 # other = classif[~classif['Lemma'].isin(nouns['Lemma'])]
@@ -249,6 +283,4 @@ woorden = pd.concat([verben, naamwoorden, adjectieven])
 # one_count = other[other['Lemma'].isin(pos_count[pos_count == 1].index)]
 # main = nouns.append(one_count)
 
-# 
-
-
+#
